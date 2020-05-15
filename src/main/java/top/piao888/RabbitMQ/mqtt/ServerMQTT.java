dadfd620691837c3346020c313c4399e209a3a5f
SDGC <hongzhi.xu@SDGC.com>
2020-05-15 11:23:45 +0800

    改了一小点没用的

--- src/main/java/top/piao888/RabbitMQ/mqtt/ServerMQTT.java
+++ src/main/java/top/piao888/RabbitMQ/mqtt/ServerMQTT.java
@@ -14,9 +14,9 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
  * 发送消息
  */
 public class ServerMQTT {
-
     //tcp://MQTT安装的服务器地址:MQTT定义的端口号
     public static final String HOST = "tcp://47.104.92.214:1883";
+//    public static final String HOST = "tcp://192.168.1.114:1883";
     //定义一个主题
 //    public static final String TOPIC = "text/top";
     //往taxt主题上面发送消息
@@ -27,6 +27,8 @@ public class ServerMQTT {
 
     private MqttClient client;
     private MqttTopic topic11;
+//    private String userName = "test";  //非必须
+//    private String passWord = "test";  //非必须
     private String userName = "BigRich";  //非必须
     private String passWord = "123";  //非必须
     private MqttMessage message;
@@ -86,29 +88,38 @@ public class ServerMQTT {
      * @throws MqttException
      */
     public static void main(String[] args) throws MqttException, InterruptedException {
-        ServerMQTT server = new ServerMQTT();
-        server.message = new MqttMessage();
-        server.message.setQos(2);  //保证消息能到达一次
-        server.message.setRetained(true);
-        server.message.setPayload("abcde1".getBytes());
+        try {
+
+
+            ServerMQTT server = new ServerMQTT();
+            server.message = new MqttMessage();
+            server.message.setQos(2);  //保证消息能到达一次
+            server.message.setRetained(true);
+            server.message.setPayload("abcde1".getBytes());
 //        server.publish(server.topic11 , server.message);
 //        Thread.sleep(300000);
-        Thread.sleep(2000);
-        server.message.setPayload("abcde2".getBytes());
+            Thread.sleep(2000);
+            server.message.setPayload("abcde2".getBytes());
 //        server.publish(server.topic11 , server.message);
-        int i = 0;
-        for (; ; ) {
+            int i = 0;
+            for (; ; ) {
 //            String message = "{\"DevNum\":1,\"SN_\":\"51911220010255\",\"Datetime\":1588993563,\"RealTime-Data\":{\"Ua\":220.1,\"Ub\":0,\"Uc\":0,\"Ia\":0.04,\"Ib\":0.04,\"Ic\":0.05,\"In\":0.08,\"P\":158.98,\"Q\":0,\"PF\":1,\"Wh\":20.67,\"Varh\":2.77,\"Ta\":6503.6,\"Tb\":6503.6,\"Tc\":6503.6,\"FWh\":19.08,\"BWh\":1954}} ";
 //
-            String message="{\"DevNum\":1,\"SN_\":\"51911220010256\",\"Datetime\":1588993563,\"RealTime-Data\":{\"Ua\":220.1,\"Ub\":0,\"Uc\":0,\"Ia\":0.04,\"Ib\":0.04,\"Ic\":0.05,\"In\":0.08,\"P\":185.0,\"Q\":0,\"PF\":1,\"Wh\":20.67,\"Varh\":2.77,\"Ta\":6503.6,\"Tb\":6503.6,\"Tc\":6503.6,\"FWh\":19.08,\"BWh\":1954}} ";
-            String message1="{\"DevNum\":2,\"SN_\":\"51911220010256\",\"Datetime\":1588993563,\"RealTime-Data\":{\"Ua\":220.1,\"Ub\":0,\"Uc\":0,\"Ia\":0.04,\"Ib\":0.04,\"Ic\":0.05,\"In\":0.08,\"P\":50,\"Q\":0,\"PF\":1,\"Wh\":20.67,\"Varh\":2.77,\"Ta\":6503.6,\"Tb\":6503.6,\"Tc\":6503.6,\"FWh\":19.08,\"BWh\":1954}} ";
+                String message = "{\"DevNum\":1,\"SN_\":\"51911220010256\",\"Datetime\":1588993563,\"RealTime-Data\":{\"Ua\":220.1,\"Ub\":0,\"Uc\":0,\"Ia\":0.04,\"Ib\":0.04,\"Ic\":0.05,\"In\":0.08,\"P\":185.0,\"Q\":0,\"PF\":1,\"Wh\":20.67,\"Varh\":2.77,\"Ta\":6503.6,\"Tb\":6503.6,\"Tc\":6503.6,\"FWh\":19.08,\"BWh\":1954}} ";
+                String message1 = "{\"DevNum\":2,\"SN_\":\"51911220010256\",\"Datetime\":1588993563,\"RealTime-Data\":{\"Ua\":220.1,\"Ub\":0,\"Uc\":0,\"Ia\":0.04,\"Ib\":0.04,\"Ic\":0.05,\"In\":0.08,\"P\":50,\"Q\":0,\"PF\":1,\"Wh\":20.67,\"Varh\":2.77,\"Ta\":6503.6,\"Tb\":6503.6,\"Tc\":6503.6,\"FWh\":19.08,\"BWh\":1954}} ";
 
-            server.message.setPayload(message.getBytes());
-            server.publish(server.topic11, server.message);
-            server.message.setPayload(message1.getBytes());
-            server.publish(server.topic11, server.message);
-//            i++;
+                server.message.setPayload(message.getBytes());
+                server.publish(server.topic11, server.message);
+                server.message.setPayload(message1.getBytes());
+                server.publish(server.topic11, server.message);
+                i++;
+                if(i==100000){
+                    break;
+                }
+            }
+        } catch (ArithmeticException e) {
         }
+        System.exit(0);
 //        System.out.println(server.message.isRetained() + "------ratained状态");
     }
 }
